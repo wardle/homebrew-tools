8f265465b23320711a7639fab52c727d5683c7b9
Mark Wardle <mark@wardle.org>
2026-04-08 14:21:58 +0100

    hermes 1.4.1575

--- Formula/hermes.rb
+++ Formula/hermes.rb
@@ -1,9 +1,9 @@
 class Hermes < Formula
   desc "SNOMED CT terminology server, library and tools"
   homepage "https://github.com/wardle/hermes"
-  url "https://github.com/wardle/hermes/releases/download/v1.4.1563/hermes-1.4.1563-lucene10.jar"
-  version "1.4.1563"
-  sha256 "26706d3e10a350c0f8eaadfb9494bde2eb8a62808fd971d1ba71f7bfa8656fff"
+  url "https://github.com/wardle/hermes/releases/download/v1.4.1575/hermes-1.4.1575-lucene10.jar"
+  version "1.4.1575"
+  sha256 "c3d5b760dccbf40f57a190d9a300b587121cd1b8b1cc2a0277ca86b9b09e859c"
   license "EPL-2.0"
 
   depends_on "openjdk"
